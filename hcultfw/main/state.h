a2725362ba6af5af0933d85834df81eddc368c85
Alex Southgate <southgateJA@cardiff.ac.uk>
2026-01-06 08:51:10 +0000

    Add forgotten state header

---
+++ hcultfw/main/state.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdint.h>
+
+#include "esp_adc/adc_oneshot.h"
+#include "host/ble_uuid.h"
+
+struct FirmwareState {
+  ble_uuid128_t service_uuid;
+  ble_uuid128_t characteristic_uuid;
+  bool ble_uuid_ok;
+  uint16_t gatt_chr_handle;
+  int sensor_value_1;
+  int sensor_value_2;
+  int64_t boot_time_us;
+  adc_oneshot_unit_handle_t adc_handle;
+  uint8_t ble_addr_type;
+  volatile bool request_sleep;
+  volatile bool sent_payload;
+};
