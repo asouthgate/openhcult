2639ddcf0c894ee6aaa47659038212136bb03589
Alex Southgate <southgateJA@cardiff.ac.uk>
2026-01-23 09:00:06 +0000

    remove tools/ in favor of unified hcultutils

--- tools/plot_timeseries/plot_timeseries.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Backwards-compatible wrapper for hcultutils.plot_timeseries."""
-
-from hcultutils.plot_timeseries import main
-
-
-if __name__ == "__main__":
-    raise SystemExit(main())
