f86246378e5ec8457be394d05753e36ab10e8ace
Alex Southgate <joelsouthgate@googlemail.com>
2026-02-21 15:33:46 +0000

    Increase noise

--- scripts/simulate_calibration.py
+++ scripts/simulate_calibration.py
@@ -53,8 +53,6 @@ def derivative_gp_simulation(x, dy_noisy, y_xmax, inv_response_prior):
     # -----------------------------
     # 4. Integrate samples
     # -----------------------------
-    # dx_test = X_test[1] - X_test[0]
-    # f_samples = np.cumsum(dy_samples, axis=0) * dx_test
 
     X = X_test.flatten()
     L = X[-1] - X[0]
@@ -70,17 +68,9 @@ def derivative_gp_simulation(x, dy_noisy, y_xmax, inv_response_prior):
     print(y_xmax)
     f_samples += y_xmax - f_samples[0, :]
 
-    # 3) RIGHT anchor (per-sample): enforce Z(Xmax) = Zmin
-    # end_err = f_samples[-1, :] - 0.0     # error at right endpoint for each sample
-    # f_samples -= ramp[:, None] * end_err[None, :]
-
     # now uncertainty is 0 at both ends
     f_mean = np.mean(f_samples, axis=1)
     f_std  = np.std(f_samples, axis=1)
-
-    # # Compute mean and std of reconstructed function
-    # f_mean = np.mean(f_samples, axis=1)
-    # f_std = np.std(f_samples, axis=1)
 
     return X_test, dy_samples, f_mean, f_std
 
@@ -97,13 +87,10 @@ def _plot_arrows(zmids, xss, dzdxs, ax, step, reverse_arrow):
         x2 = xmid - 0.5 * step
         z1 = zmid + 0.5 * step * dzdx
         z2 = zmid - 0.5 * step * dzdx
-        # ax[0].plot([x1, x2], [z1, z2], color='red', alpha=0.5)
         # add an arrowhead
         if reverse_arrow:
-            # ax.arrow(x1, z1, x2-x1, z2-z1, head_width=5, head_length=10, fc='black', ec='black', alpha=0.5)
             ax.annotate("", xy=(x2, z2), xytext=(x1, z1), arrowprops=prop, alpha=0.5)
         else:
-            # ax.arrow(x2, z2, x1-x2, z1-z2, head_width=5, head_length=10, fc='black', ec='black', alpha=0.5) 
             ax.annotate("", xy=(x1, z1), xytext=(x2, z2), arrowprops=prop, alpha=0.5)
         ax.scatter(xmid, zmid, color='black', s=10)
 
@@ -114,36 +101,22 @@ def get_gamma_shape_scale(z, Zmax, varZ0, varZmax):
     return shape, scale
 
 def sample_gamma_func(z, Zmax, varZ0, varZmax):
-    # lerp the beta value from varZ0 to varZmax
-    # varZ = varZ0 + (varZmax - varZ0) * (z / Zmax)
     shape, scale = get_gamma_shape_scale(z, Zmax, varZ0, varZmax)
     return np.random.gamma(shape, scale)
 
 def get_gamma_pdf(x, z, Zmax, varZ0, varZmax):
-    # varZ = varZ0 + (varZmax - varZ0) * (z / Zmax)
-    # shape = 1.0 / varZ
-    # scale = varZ
     shape, scale = get_gamma_shape_scale(z, Zmax, varZ0, varZmax)
     return gamma.pdf(x=x, a=shape, scale=scale)
 
 def get_gamma_mean(z, Zmax, varZ0, varZmax):
-    # varZ = varZ0 + (varZmax - varZ0) * (z / Zmax)
-    # shape = 1.0 / varZ
-    # scale = varZ
     shape, scale = get_gamma_shape_scale(z, Zmax, varZ0, varZmax)
     return shape * scale
 
 def get_gamma_95_percent_ci(z, Zmax, varZ0, varZmax):
-    # varZ = varZ0 + (varZmax - varZ0) * (z / Zmax)
-    # shape = 1.0 / varZ
-    # scale = varZ
     shape, scale = get_gamma_shape_scale(z, Zmax, varZ0, varZmax)
     return gamma.ppf(0.95, a=shape, scale=scale)
 
 def get_gamma_05_percent_ci(z, Zmax, varZ0, varZmax):
-    # varZ = varZ0 + (varZmax - varZ0) * (z / Zmax)
-    # shape = 1.0 / varZ
-    # scale = varZ
     shape, scale = get_gamma_shape_scale(z, Zmax, varZ0, varZmax)
     return gamma.ppf(0.05, a=shape, scale=scale)
 
@@ -158,15 +131,11 @@ if __name__ == "__main__":
     sigma2_x = 0.0
     X_at_Zmin = 2000
     Z_at_Xmax = 0.0
-    alpha_W = 2.0
     n_samps_per_sensor = 50
-    n_sensors = 2   
+    n_sensors = 3
 
-    gamma_var_zmin = 0.4
+    gamma_var_zmin = 0.8
     gamma_var_zmax = 0.001
-    # X_at_Zmax = 300
-    # X_unscaled = np.random.uniform(X_at_Zmax, X_at_Zmin, size=100)
-    # response_func_z = lambda z: X_at_Zmax + decreasing_logistic(z, mid= 0.5 * Zmax, L=X_at_Zmin, k=0.05)
 
     # response_func_z = lambda z: X_at_Zmin  - (5.2/Zmax) * z - (10.2/Zmax) * z**2 + (0.01/Zmax) * z**3
     # set response func to an exponential intersecting at X_at_Zmin
@@ -191,10 +160,10 @@ if __name__ == "__main__":
         # dzdxs = []
         zmids_.append(z + 0.5 * W)
         for sensor in range(n_sensors):
-            if alpha_W:
-                M = sample_gamma_func(z, Zmax, gamma_var_zmin, gamma_var_zmax)
-            else:
-                M = 1.0
+            # if alpha_W:
+            M = sample_gamma_func(z, Zmax, gamma_var_zmin, gamma_var_zmax)
+            # else:
+            #     M = 1.0
             assert M > 0
             W_scaled = W * M
             assert W_scaled > 0
@@ -205,8 +174,9 @@ if __name__ == "__main__":
             dxs.append(dx_)
             # dzdxs.append(wdx_)
         dxs_all.append(dxs)
-        dzdx = W / (sum(dxs) / len(dxs))
-        dx = sum(dxs) / len(dxs)
+        # dx = sum(dxs) / len(dxs)
+        dx = np.median(dxs)
+        dzdx = W / dx
         xs_.append(rfz + 0.5 * dx)
         dzdx_.append(dzdx)
         dxs_.append(dx)
@@ -246,7 +216,6 @@ if __name__ == "__main__":
     for sdi, sensor_dxs in enumerate(dxs_all):
         z = zs_[sdi] 
         for si, sdx in enumerate(sensor_dxs):
-            # print(si)    
             # plot vertical line of length sdx at position z, centered on response(z)
             response = response_func_z(z)
             ax[1].plot([z, z], [response, response+sdx], color=colors[si], alpha=1.0, label=f"Sensor {si}" if sdi == 0 else None)
@@ -258,7 +227,6 @@ if __name__ == "__main__":
     for sdi, sensor_dxs in enumerate(dxs_all):
         z = zs_[sdi] 
         for si, sdx in enumerate(sensor_dxs):
-            # print(si)    
             # plot vertical line of length sdx at position z, centered on response(z)
             response = response_func_z(z)
             ax[2].plot([response+sdx, response+sdx], [z, z+W], color=colors[si], alpha=1.0, label=f"Sensor {si}" if sdi == 0 else None)
@@ -270,40 +238,23 @@ if __name__ == "__main__":
 
     ax[3].plot([response_func_z(z) for z in zs_], zs_, color='blue', alpha=0.5)
     _plot_arrows(zmids_, xs_, dzdx_, ax[3], W * 5, True)
-    # for zi, zmid in enumerate(zmids_): 
-    #     dzdx = dzdx_[zi]
-    #     xmid = xs_[zi]
-    #     # compute little line segment forward and back using xmid, zmid, and dzdx
-    #     step = W * 10
-    #     x1 = xmid + 0.5 * step
-    #     x2 = xmid - 0.5 * step
-    #     z1 = zmid + 0.5 * step * dzdx
-    #     z2 = zmid - 0.5 * step * dzdx
-    #     # ax[0].plot([x1, x2], [z1, z2], color='red', alpha=0.5)
-    #     # add an arrowhead
-    #     ax[0].arrow(x1, z1, x2-x1, z2-z1, head_width=5, head_length=10, fc='red', ec='red', alpha=0.5)
-    #     ax[0].scatter(xmid, zmid, color='red', s=10)
-    # ax[0].scatter(sorted_xs_,sorted_zmids, s=5.0)
+    
     ax[3].set_xlabel("$X$ ")
     ax[3].set_ylabel("Z")
 
     ax[4].plot(zs_, [response_func_z(z) for z in zs_])
-    # ax[1].scatter(sorted_zmids, sorted_xs_, s=5.0)
     ax[4].set_xlabel("Z")
     ax[4].set_ylabel("$X$")
     _plot_arrows(xs_, zmids_, 1.0/np.array(dzdx_), ax[4], 5.0, False)
 
-#    ax[2].plot(sorted_zmids, sorted_dzdx_)
     ax[5].scatter(sorted_zmids, sorted_dzdx_)
     ax[5].set_xlabel("Z")
     ax[5].set_ylabel("$dZ/dX$")
 
-#    ax[3].plot(sorted_xs_,  sorted_dzdx_)
     ax[6].scatter(sorted_xs_, sorted_dzdx_)
     ax[6].set_xlabel("X")
     ax[6].set_ylabel("$dZ/dX$")
 
-#    ax[4].plot(sorted_xs_, sorted_dzdx_)
     ax[7].scatter(sorted_xs_, sorted_dzdx_, label="Sampled derivative data")
     dmean = np.mean(dy_samples, axis=1)
     dstd = np.std(dy_samples, axis=1)
@@ -315,11 +266,10 @@ if __name__ == "__main__":
         alpha=0.3,
         label="GP 95% CI",
     )
-    ax[7].set_xlabel("X")
-    ax[7].set_ylabel("f'(X)")
+    ax[7].set_xlabel("$X$")
+    ax[7].set_ylabel("$f'(X)$")
     ax[7].legend()
 
-    # ax[5].scatter(sorted_xs_, sorted_zmids, s=5.0, alpha=0.5)
     ax[8].plot([response_func_z(z) for z in zs_], zs_, label="True response curve")
     ax[8].plot(X_test.flatten(), f_mean, color = 'orange', label="Integrated GP mean")
     ax[8].fill_between(
@@ -328,10 +278,9 @@ if __name__ == "__main__":
         f_mean + 2 * f_std,
         alpha=0.3, color = 'orange', label="Integrated GP samples at 95% CI",
     )
-    ax[8].set_xlabel("X")
-    ax[8].set_ylabel("f(X)")
+    ax[8].set_xlabel("$X$")
+    ax[8].set_ylabel("$f(X)$")
     ax[8].legend()
     plt.suptitle("GP regression on derivatives with integration to reconstruct response curve")
     plt.savefig("simulation_example.png", dpi=300)
-    # plt.tight_layout(pad=2.0)
     plt.show()
