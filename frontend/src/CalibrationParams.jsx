6932fd29a1fded53298350974be9c35e6b58b401
Alex Southgate <joelsouthgate@googlemail.com>
2026-04-14 15:31:45 +0100

    Add missing jsx file

---
+++ frontend/src/CalibrationParams.jsx
@@ -0,0 +1,36 @@
+export default function CalibrationParams({
+  offsetMin, setOffsetMin, widthMin, setWidthMin,
+  gpStdMl, setGpStdMl, scalePriorMean, setScalePriorMean, scalePriorStd, setScalePriorStd,
+}) {
+  return (
+    <table className="param-table">
+      <tbody>
+        <tr>
+          <td>offset</td>
+          <td><input type="number" min="0" value={offsetMin} onChange={e => setOffsetMin(Number(e.target.value))} style={{ width: 52 }} /></td>
+          <td>min</td>
+        </tr>
+        <tr>
+          <td>width</td>
+          <td><input type="number" min="1" value={widthMin} onChange={e => setWidthMin(Number(e.target.value))} style={{ width: 52 }} /></td>
+          <td>min</td>
+        </tr>
+        <tr>
+          <td>GP std</td>
+          <td><input type="number" min="0.1" step="0.1" value={gpStdMl} onChange={e => setGpStdMl(Number(e.target.value))} style={{ width: 52 }} /></td>
+          <td>ml</td>
+        </tr>
+        <tr>
+          <td>scale prior μ</td>
+          <td><input type="number" min="0" step="10" value={scalePriorMean} onChange={e => setScalePriorMean(e.target.value)} placeholder="off" style={{ width: 60 }} /></td>
+          <td>ml</td>
+        </tr>
+        <tr>
+          <td>scale prior σ</td>
+          <td><input type="number" min="0" step="10" value={scalePriorStd} onChange={e => setScalePriorStd(e.target.value)} placeholder="off" style={{ width: 60 }} /></td>
+          <td>ml</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
