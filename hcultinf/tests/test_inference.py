9842277bab614eddf6dc5324931bd0eec101882b
Alex Southgate <joelsouthgate@googlemail.com>
2026-04-21 22:22:25 +0100

    improve tests for exp caliabrator

--- hcultinf/tests/test_inference.py
+++ hcultinf/tests/test_inference.py
@@ -52,7 +52,7 @@ def test_convergence_in_n_bad_prior(estimator):
         priorx_pts = np.array([TEST_XMIN, TEST_XMAX])
         priory_pts = np.array([1.0, 0.0])
         errs = []
-        for _ in range(5):
+        for _ in range(10):
             x, dx, dy = simulate_calibration_data_samples(
                 TEST_XMIN,
                 TEST_XMAX,
